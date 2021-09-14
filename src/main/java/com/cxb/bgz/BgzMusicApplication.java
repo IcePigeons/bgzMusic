1d04fff0410e7ea9e2b9e2ec5921fbb5290a4c69
IcePigeons <771723358@qq.com>
2021-09-15 07:08:31 +0800

    搭建项目环境，基于springboot2.5.4

---
+++ src/main/java/com/cxb/bgz/BgzMusicApplication.java
@@ -0,0 +1,13 @@
+package com.cxb.bgz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgzMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgzMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cxb/bgz/BgzMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxb.bgz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgzMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
